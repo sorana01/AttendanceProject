f7779f943108bbb9aba734cd1fb871f151f2a573
Doris Sorana Haiduc <doris.haiduc@student.upt.ro>
2024-04-30 19:21:28 +0300

    courses fragment

---
+++ app/src/main/java/com/example/attendanceproject/ui/courses/CoursesFragment.java
@@ -0,0 +1,37 @@
+package com.example.attendanceproject.ui.courses;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.attendanceproject.databinding.FragmentCoursesBinding;
+
+public class CoursesFragment extends Fragment {
+
+    private FragmentCoursesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CoursesViewModel coursesViewModel =
+                new ViewModelProvider(this).get(CoursesViewModel.class);
+
+        binding = FragmentCoursesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textCourses;
+        coursesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
