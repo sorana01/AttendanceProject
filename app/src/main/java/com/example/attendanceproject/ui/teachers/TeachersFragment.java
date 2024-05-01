0b5928a73a785f071840d5862950730509983fa8
Doris Sorana Haiduc <doris.haiduc@student.upt.ro>
2024-05-01 15:14:52 +0300

    added lists to student and teacher fragments
    and displayed the students and teachers who were approved by admin

--- app/src/main/java/com/example/attendanceproject/ui/teachers/TeachersFragment.java
+++ app/src/main/java/com/example/attendanceproject/ui/teachers/TeachersFragment.java
@@ -4,29 +4,67 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.attendanceproject.databinding.FragmentStudentsBinding;
 import com.example.attendanceproject.databinding.FragmentTeachersBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeachersFragment extends Fragment {
 
     private FragmentTeachersBinding binding;
+    private FirebaseFirestore fStore;
+    private ListView usersListView;
+    private ArrayAdapter<String> adapter;
+    private List<String> userList = new ArrayList<>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fStore = FirebaseFirestore.getInstance(); // Initialize Firestore once
+        loadUsers(); // Load users initially here, only once when the fragment is first created
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        TeachersViewModel teachersViewModel =
-                new ViewModelProvider(this).get(TeachersViewModel.class);
-
         binding = FragmentTeachersBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        return binding.getRoot();
+    }
 
-        final TextView textView = binding.textTeachers;
-        teachersViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        usersListView = binding.usersListView;
+        adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, userList);
+        usersListView.setAdapter(adapter);
+    }
+
+    private void loadUsers() {
+        fStore.collection("Users").whereEqualTo("isApproved", "true").whereEqualTo("isTeacher", true)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String userDetail = document.getString("FullName");
+                            userList.add(userDetail);
+                        }
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(getContext(), "Error loading users", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @Override
