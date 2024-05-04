8ad7113e2a960f559d671ac4def2433097d97d21
Doris Sorana Haiduc <doris.haiduc@student.upt.ro>
2024-05-04 22:47:06 +0300

    dep on option clicked, populate recyclerview
    assign students - loadStudents() called
    assign teachers - loadTeachers() called

--- app/src/main/java/com/example/attendanceproject/account/admin/CourseDetailsBottomSheetFragment.java
+++ app/src/main/java/com/example/attendanceproject/account/admin/CourseDetailsBottomSheetFragment.java
@@ -4,15 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.attendanceproject.R;
-import com.example.attendanceproject.databinding.BottomSheetCourseDetailsBinding;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CourseDetailsBottomSheetFragment extends BottomSheetDialogFragment {
+    private RecyclerView recyclerView;
+    private FirebaseFirestore fStore;
+    private CheckableEntityAdapter checkableEntityAdapter;
+    private ArrayList<EntityItem> userList = new ArrayList<>();
+    private LinearLayout toolbarLayout;
+    private LinearLayout optionsLayout;
+    private LinearLayout contentArea;
 
     public static CourseDetailsBottomSheetFragment newInstance() {
         return new CourseDetailsBottomSheetFragment();
@@ -22,6 +38,7 @@ public class CourseDetailsBottomSheetFragment extends BottomSheetDialogFragment 
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setStyle(STYLE_NORMAL, R.style.CustomBottomSheetDialog); // Apply the custom style here
+        fStore = FirebaseFirestore.getInstance();
     }
 
     @Nullable
@@ -41,13 +58,68 @@ public class CourseDetailsBottomSheetFragment extends BottomSheetDialogFragment 
             // Use courseName and courseDetail as needed
         }
 
+        recyclerView = view.findViewById(R.id.recyclerView);
+        toolbarLayout = view.findViewById(R.id.toolbarLayout);
+        optionsLayout = view.findViewById(R.id.optionsLayout);
+        checkableEntityAdapter = new CheckableEntityAdapter(getContext(), userList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(checkableEntityAdapter);
+
         view.findViewById(R.id.assignStudentsTV).setOnClickListener(v -> {
             // TODO: Implement selection logic or show another dialog to select teachers
+//            showRecyclerView(view, "isStudent");
+            loadUsers("isStudent");
+            recyclerView.setVisibility(View.VISIBLE);
+            optionsLayout.setVisibility(View.GONE);
+            toolbarLayout.setVisibility(View.VISIBLE);
         });
 
         view.findViewById(R.id.assignTeachersTV).setOnClickListener(v -> {
             // TODO: Implement selection logic or show another dialog to select students
+//            showRecyclerView(view, "isTeacher");
+            loadUsers("isTeacher");
+            recyclerView.setVisibility(View.VISIBLE);
+            optionsLayout.setVisibility(View.GONE);
+            toolbarLayout.setVisibility(View.VISIBLE);
         });
 
+        Button backButton = view.findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> {
+            toolbarLayout.setVisibility(View.GONE);
+            optionsLayout.setVisibility(View.VISIBLE);
+            recyclerView.setVisibility(View.GONE);
+        });
+
+        Button saveButton = view.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(v -> {
+            saveData(); // Implement saving logic
+            recyclerView.setVisibility(View.GONE);
+            toolbarLayout.setVisibility(View.GONE);
+            optionsLayout.setVisibility(View.VISIBLE);
+        });
+
+    }
+
+    private void saveData() {
+
+    }
+
+    private void loadUsers(String isRole) {
+        fStore.collection("Users").whereEqualTo("isApproved", "true").whereEqualTo(isRole, true)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        userList.clear();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String userName = document.getString("FullName");
+                            String userDetail = document.getString("UserEmail");
+                            CheckableEntityItem item = new CheckableEntityItem(userName, userDetail);
+                            userList.add(item);
+                        }
+                        checkableEntityAdapter.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(getContext(), "Error loading users", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
