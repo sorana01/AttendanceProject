5899680a957c7ae6f8229d1460c71b7779beba7f
Doris Sorana Haiduc <doris.haiduc@student.upt.ro>
2024-05-05 13:07:39 +0300

    Added comments to EntityAdapter

--- app/src/main/java/com/example/attendanceproject/account/admin/EntityAdapter.java
+++ app/src/main/java/com/example/attendanceproject/account/admin/EntityAdapter.java
@@ -29,11 +29,21 @@ public class EntityAdapter<T extends EntityItem> extends RecyclerView.Adapter<En
         this.listener = listener;
     }
 
+    /**
+     * Initialize the dataset of the Adapter
+     *
+     * @param entityItems ArrayList<T> containing the data to populate views to be used
+     * by RecyclerView
+     */
     public EntityAdapter(Context context, ArrayList<T> entityItems) {
         this.entityItems = entityItems;
         this.context = context;
     }
 
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
     public static class EntityViewHolder extends RecyclerView.ViewHolder{
 
         private TextView entityName;
@@ -42,22 +52,29 @@ public class EntityAdapter<T extends EntityItem> extends RecyclerView.Adapter<En
 
         public EntityViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            // Define click listener for the ViewHolder's View
             entityName = itemView.findViewById(R.id.entity_name_textview);
             entityDetail = itemView.findViewById(R.id.entity_detail_textview);
         }
     }
 
 
+    // Create new views (invoked by the layout manager)
     @NonNull
     @Override
     public EntityViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Create a new view, which defines the UI of the list item
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.entity_item, parent, false);
         return new EntityViewHolder(itemView);
     }
 
+    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(@NonNull EntityViewHolder holder, int position) {
         T item = entityItems.get(position);
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
         holder.entityName.setText(entityItems.get(position).getEntityName());
         holder.entityDetail.setText(entityItems.get(position).getEntityDetail());
 
@@ -68,6 +85,7 @@ public class EntityAdapter<T extends EntityItem> extends RecyclerView.Adapter<En
         });
     }
 
+    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return entityItems.size();
